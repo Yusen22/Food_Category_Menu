ea082864c8483ade6989e60cc9417b7ddb688430
Elijah WIlsher <elijahwilsher@gmail.com>
2024-07-28 23:13:24 +0700

    created working MenuItem component

--- src/MenuItem.jsx
+++ src/MenuItem.jsx
@@ -1,6 +1,17 @@
-const MenuItem = () => {
+const MenuItem = (item) => {
+  const { img, title, category, price, desc, id, key } = item
   return (
-    <div>MenuItem</div>
+    <article key={key} className="menu-item">
+      <img className="img" src={img} />
+      <div className="item-info">
+        <header className="">
+          <h5>{title}</h5>
+          <p className="item-price">{price}</p>
+        </header>
+        <p className="item-text">{desc}</p>
+      </div>
+
+    </article>
   )
 }
 export default MenuItem
