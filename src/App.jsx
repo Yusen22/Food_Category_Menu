4767fddab43d4cde13902a40642428128c582055
Elijah WIlsher <elijahwilsher@gmail.com>
2024-07-28 19:23:59 +0700

    imported Title to page and rendered

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,17 @@
+import { useState } from "react";
+
+import data from './data'
+
+import Title from "./Title";
+
 const App = () => {
-  return <h2>Menu Starter</h2>;
+  const [menuItems, setMenuItems] = useState(data)
+  return (
+    <main>|
+      <Title>Our menu</Title>
+
+      
+    </main>
+  )
 };
 export default App;
