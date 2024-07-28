74047eebb63a173b9dcd1f6f38e2f4d92e60e95c
Elijah WIlsher <elijahwilsher@gmail.com>
2024-07-28 23:13:10 +0700

    created Menu and rendered MenuItems based on data

--- src/Menu.jsx
+++ src/Menu.jsx
@@ -1,6 +1,17 @@
-const Menu = () => {
+import MenuItem from "./MenuItem"
+
+const Menu = ({menuItems}) => {
+  console.log(menuItems)
   return (
-    <div>Menu</div>
+    <section className="menu section-center">
+    {menuItems.map((item, i) => {
+      console.log(item)
+      return (
+        <MenuItem key={item.id} {...item}/>
+      )
+    })}
+    
+    </section>
   )
 }
 export default Menu
