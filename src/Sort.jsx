599c9a10ef80fb5db1834bd03bf2f3439f7993f5
Elijah WIlsher <elijahwilsher@gmail.com>
2024-07-29 03:09:48 +0700

    added full sort functionality

--- src/Sort.jsx
+++ src/Sort.jsx
@@ -1,6 +1,16 @@
-const Sort = () => {
+import menu from "./data"
+
+const Sort = ({menuItems, sortList}) => {
+    console.log(menuItems)
+    const menuKeys = Object.keys(menuItems[0])
+    console.log(menuKeys)
   return (
-    <div>Sort</div>
+    <div className="sort-container">
+        <p>Sort by:</p>
+        <p onClick={() => sortList(menuKeys[1])} className="sort-item">{menuKeys[1]}</p>
+        <p onClick={() => sortList(menuKeys[2])} className="sort-item">{menuKeys[2]}</p>
+        <p onClick={() => sortList(menuKeys[3])} className="sort-item">{menuKeys[3]}</p>
+    </div>
   )
 }
 export default Sort
