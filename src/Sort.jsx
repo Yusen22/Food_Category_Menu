54acf03865c5c724e765012601abc43a695b11aa
Elijah WIlsher <elijahwilsher@gmail.com>
2024-07-29 05:27:28 +0700

    added Sort functional component which sorts in each category for title, category and price

--- src/Sort.jsx
+++ src/Sort.jsx
@@ -1,18 +1,46 @@
-import menu from "./data"
 
-const Sort = ({menuItems, originalMenu, sortList}) => {
-    console.log(menuItems)
+import { useState, useEffect } from "react"
+
+const Sort = ({ displayActive, activeCat, menuItems, setMenuItems, originalMenu, activeSort, setActiveSort, sortList }) => {
+   
     const menuKeys = Object.keys(menuItems[0])
-    console.log(menuKeys)
-  return (
-    <div className="sort-container">
-        <p>Sort by:</p>
-        <p onClick={() => sortList(menuKeys[1])} className="sort-item">{menuKeys[1]}</p>
-        {menuItems === originalMenu ? (
-            <p onClick={() => sortList(menuKeys[2])} className="sort-item">{menuKeys[2]}</p>
-        ) : ''}
-        <p onClick={() => sortList(menuKeys[3])} className="sort-item">{menuKeys[3]}</p>
-    </div>
-  )
+
+    const handleSortClick = (indx) => {
+        const key = menuKeys[indx]
+        console.log(key)
+        sortList(key)
+        console.log(activeCat)
+
+        setActiveSort(prevActiveSort => {
+            if (prevActiveSort.includes(key)) {
+                displayActive(activeCat)
+                return prevActiveSort.filter(item => item !== key)
+            } else if (prevActiveSort.lenqth === 0) {
+                return [key]
+            } else {
+                return [key]
+            }
+        })
+    }
+    useEffect(() => {
+        console.log(activeSort)
+    }, [activeSort])
+
+    const sortClass = (indx) => {
+        return (
+            activeSort.includes(menuKeys[indx]) ? 'active-sort sort-item' : 'sort-item'
+        )
+    }
+
+    return (
+        <div className="sort-container">
+            <p>Sort by:</p>
+            <p className={sortClass(1)} onClick={() => handleSortClick(1)}>{menuKeys[1]}</p>
+            {menuItems.length === originalMenu.length ? (
+                <p  className={sortClass(2)} onClick={() => handleSortClick(2)}>{menuKeys[2]}</p>
+            ) : ''}
+            <p className={sortClass(3)} onClick={() => handleSortClick(3)}>{menuKeys[3]}</p>
+        </div>
+    )
 }
 export default Sort
