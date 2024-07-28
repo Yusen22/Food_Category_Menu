cd2377c21bff4c8eb2221943d8c4d9bba84e6051
Elijah WIlsher <elijahwilsher@gmail.com>
2024-07-28 23:11:28 +0700

    added getCategories fnctn to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import menu from './data'
 
 import Title from "./Title";
 import Menu from "./Menu";
-import MenuItem from "./MenuItem";
+import CategoryButtons from "./CategoryButtons";
 
 const App = () => {
   const [menuItems, setMenuItems] = useState(menu)
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => {
+    const getCategories = () => {
+      const catSet = new Set(menuItems.map(item => item.category));
+      const catSetArray = Array.from(catSet)
+      catSetArray.unshift('all')
+      setCategories(catSetArray)
+    }
+    getCategories()
+  }, [menuItems])
+  
+
+  
   return (
     <main>|
       <Title>Our menu</Title>
+      <CategoryButtons category = {categories} />
+      <Menu menuItems = {menuItems}/>
 
-      
+
     </main>
   )
 };
