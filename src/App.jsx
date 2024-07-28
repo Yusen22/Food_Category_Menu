cbc5015abb79d0ebc00eb93bf2b6c4826e68b041
Elijah WIlsher <elijahwilsher@gmail.com>
2024-07-29 01:29:13 +0700

    added displayActive function

--- src/App.jsx
+++ src/App.jsx
@@ -7,25 +7,42 @@ import Menu from "./Menu";
 import CategoryButtons from "./CategoryButtons";
 
 const App = () => {
+  const [originalMenu, setOriginalMenu] = useState(menu)
   const [menuItems, setMenuItems] = useState(menu)
   const [categories, setCategories] = useState([])
+  const [activeCat, setActiveCat] = useState('')
+
+
 
   useEffect(() => {
     const getCategories = () => {
       const catSet = new Set(menuItems.map(item => item.category));
       const catSetArray = Array.from(catSet)
       catSetArray.unshift('all')
+      console.log(catSetArray)
       setCategories(catSetArray)
     }
     getCategories()
-  }, [menuItems])
-  
+    
+  }, [originalMenu])
 
-  
+const displayActive = (cat) => {
+  setActiveCat(cat)
+  console.log(cat)
+  if (cat === 'all') {
+    return setMenuItems(originalMenu)
+  } else {
+  const filteredMenu = originalMenu.filter(result => result.category === cat)
+  console.log(filteredMenu)
+  setMenuItems(filteredMenu)
+  }
+
+}
+
   return (
     <main>|
       <Title>Our menu</Title>
-      <CategoryButtons category = {categories} />
+      <CategoryButtons category = {categories} displayActive={displayActive} />
       <Menu menuItems = {menuItems}/>
 
 
